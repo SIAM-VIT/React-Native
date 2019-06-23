bcfdac5f1f0280c736d919114db35f1f87696916
HrithikMittal <adhikanshmittal@gmail.com>
2019-06-23 19:50:30 +0530

    class component

--- albums/index.js
+++ albums/index.js
@@ -1,10 +1,18 @@
 // Import a library to create a component
 import React from "react";
-import { AppRegistry } from "react-native";
+import { AppRegistry, View } from "react-native";
 import Header from "./src/components/header";
+import AlbumList from "./src/components/AlbumList";
 
 // Create a component
-const App = () => <Header headerText={"Albums"} />;
+const App = () => {
+  return (
+    <View>
+      <Header headerText={"Albums"} />
+      <AlbumList />
+    </View>
+  );
+};
 
 // Render it to the device
 AppRegistry.registerComponent("albums", () => App);
---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+// Class Component
+class AlbumList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Album List !!!!</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
