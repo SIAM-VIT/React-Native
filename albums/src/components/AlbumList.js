bc254c16876a68f1a7967b7115965f06e89f7803
HrithikMittal <adhikanshmittal@gmail.com>
2019-06-23 21:36:45 +0530

    class component

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import axios from "axios";
 
 // Class Component
 class AlbumList extends Component {
+  componentWillMount() {
+    axios
+      .get("https://rallycoding.herokuapp.com/api/music_albums")
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.log("Error is ", err);
+      });
+  }
+
   render() {
     return (
       <View>
