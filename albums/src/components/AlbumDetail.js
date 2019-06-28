7a27e894a1b95ed9f8680f33b6ae14c344c34d10
HrithikMittal <adhikanshmittal@gmail.com>
2019-06-28 16:17:57 +0530

    image rendering

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,16 +1,36 @@
 import React from "react";
-import { Text, Image } from "react-native";
+import { View, Text, Image } from "react-native";
 import Card from "./Card";
 import CardSection from "./CardSection";
 
-const AlbumDetail = props => {
+const AlbumDetail = ({ record }) => {
+  const { title, artist, thumbnail_image } = record;
+  const { thumbnailStyle, headerContentStyle } = styles;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.record.title}</Text>
+        <View>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: "row",
+    justifyContent: "flex-start"
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
 };
 
 export default AlbumDetail;
--- albums/src/components/CardSection.js
+++ albums/src/components/CardSection.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View } from "react-native";
 
-const CardSection = () => {
+const CardSection = props => {
   return <View styles={style.containerStyle}>{props.children}</View>;
 };
 
