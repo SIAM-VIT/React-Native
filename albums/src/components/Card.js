b1174099209ac3a0380c4ef4de17edede685cb46
HrithikMittal <adhikanshmittal@gmail.com>
2019-06-28 14:38:31 +0530

    desgining a card

--- albums/src/components/Card.js
+++ albums/src/components/Card.js
@@ -2,7 +2,24 @@ import React from "react";
 import { View } from "react-native";
 
 const Card = () => {
-  return <View />;
+  return <View style={styles.containerStyle} />;
+};
+
+const styles = {
+  containerStyle: {
+    borderWidth: 1,
+    borderRadius: 2,
+    boderColor: "#ddd",
+    borderBottomWidth: 0,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadworRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10
+  }
 };
 
 export default Card;
