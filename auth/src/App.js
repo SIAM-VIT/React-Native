592b27dc5a289457f0ff02660a69959da59f71b6
Adhikansh Mittal <adhikanshmittal@gmail.com>
2019-06-30 16:30:27 +0530

    login and logout

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,34 +1,56 @@
 import React,{Component} from 'react';
 import {View,Text} from 'react-native';
 import firebase from 'firebase';
-import {Header} from './components/common';
+import {Header, Button, Spinner} from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component{
+
+    state={loggedIn:null};
         //Life Cycle Method
 componentDidMount()
-    {   // Your web app's Firebase configuration
-        var firebaseConfig = {
-            apiKey: "AIzaSyBFf9wwML6O6Dpp65jd_4LO-6NWqF6fyjk",
-            authDomain: "authentication-ca1da.firebaseapp.com",
-            databaseURL: "https://authentication-ca1da.firebaseio.com",
-            projectId: "authentication-ca1da",
-            storageBucket: "",
-            messagingSenderId: "275244024522",
-            appId: "1:275244024522:web:3a4b8891c5f5b12f"
-        };
-        // Initialize Firebase
-        firebase.initializeApp(firebaseConfig);
-    }
+                {   // Your web app's Firebase configuration
+                    var firebaseConfig = {
+                        apiKey: "AIzaSyBFf9wwML6O6Dpp65jd_4LO-6NWqF6fyjk",
+                        authDomain: "authentication-ca1da.firebaseapp.com",
+                        databaseURL: "https://authentication-ca1da.firebaseio.com",
+                        projectId: "authentication-ca1da",
+                        storageBucket: "",
+                        messagingSenderId: "275244024522",
+                        appId: "1:275244024522:web:3a4b8891c5f5b12f"
+                    };
+                    // Initialize Firebase
+                    firebase.initializeApp(firebaseConfig);
 
-    render(){
-        return(
-            <View>
-                <Header headerText="Authentication" ></Header>
-                <LoginForm />
-            </View>
-        );
-    }
+                    firebase.auth().onAuthStateChanged((user)=>{
+                    if(user){
+                        this.setState({loggedIn:true});
+                    }
+                    else{
+                        this.setState({loggedIn:false});
+                    }
+                });                
+            }
+            renderContent(){
+
+                switch(this.state.loggedIn){
+                    case true:
+                        return <Button>Log Out</Button>
+                    case false:
+                        return <LoginForm />
+                    default:
+                        return <Spinner size='large' />;
+                }
+            }
+
+            render(){
+                return(
+                    <View>
+                        <Header headerText="Authentication" ></Header>
+                        {this.renderContent()}
+                    </View>
+                );0
+            }
 }
 
 export default App;
