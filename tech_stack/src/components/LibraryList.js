5c4b1e6c535d16b58fb1781e1ef0ecba2d5adad5
Adhikansh Mittal <adhikanshmittal@gmail.com>
2019-07-02 13:07:22 +0530

    mapping

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+
 class LibraryList extends Component{
     render(){
+        console.log(this.props)
         return;
     }
 };
 
-export default connect()(LibraryList);
+const mapStateToProps = state =>{
+    return {libraries:state.libraries};
+};
+
+export default connect(mapStateToProps)(LibraryList);
